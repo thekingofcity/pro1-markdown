3667d85ab78a9a678b2ad47b35c15c8c7b46cfd3
thekingofcity <1634814279@qq.com>
2017-11-08 19:35:54 +0800

    Alpha 2

--- pro.js
+++ pro.js
@@ -15,17 +15,49 @@ pro.controller('main',["$scope","$sce",'$http',function($scope,$sce,$http){
     $scope.text0=[];
     $scope.newString="start here...";
     $scope.refresh=function() {
-        var text = $scope.newString.replace(/\n/g, '<br/>\n');
-        text = text.split(/\n/g);
+        //var text = $scope.newString.replace(/\n/g, '<br/>\n').split(/\n/g);
+        var text = $scope.newString.split(/\n/g);
         for(var i=0;i<text.length;i++){
-            if(text[i].charAt(0)=="#"){
-                var j=1;
-                for(;j<6;j++){
-                    if(text[i].charAt(j)!="#"){break;}
+            if(text[i].match(/^#{1}/)){
+                if(text[i].match(/^#{2}/)){
+                    if(text[i].match(/^#{3}/)){
+                        if(text[i].match(/^#{4}/)){
+                            if(text[i].match(/^#{5}/)){
+                                if(text[i].match(/^#{6}/)){
+                                    text[i]=text[i].replace(/#{6}/,"<h6>");
+                                }else{
+                                    text[i]=text[i].replace(/#{5}/,"<h5>");
+                                }
+                            }else{
+                                text[i]=text[i].replace(/#{4}/,"<h4>");
+                            }
+                        }else{
+                            text[i]=text[i].replace(/#{3}/,"<h3>");
+                        }
+                    }else{
+                        text[i]=text[i].replace(/#{2}/,"<h2>");
+                    }
+                }else{
+                    text[i]=text[i].replace(/#{1}/,"<h1>");
                 }
-                text[i]="<h"+j+">"+text[i].substr(j)+"</h"+j+">";
-
+                text[i]=text[i].replace(/#{0,}$/,"")+"</h"+text[i].charAt(2)+">";
             }
+            var regAtx=/^#/;
+            // if(text[i].match(regAtx)){
+            //     for(var j=1;j<6;j++){
+            //         if(text[i].charAt(j)!="#"){break;}
+            //     }
+            //     text[i]="<h"+j+">"+text[i].substr(j);
+            //     for(j=text[i].length-1;j>=text[i].length-6;j--){
+            //         if(text[i].charAt(j)!="#"){break;}
+            //     }
+            //     text[i]=text[i].substr(0,j)+"</h"+j+">";
+            // }
+            // # Title
+
+            var regList = /\*\1{0,5}/;
+            // * List
+            text[i]=text[i]+"<br/>";
         }
         $scope.text0=text;
         $scope.trustHtml="";
---
+++ version.md
@@ -0,0 +1,27 @@
+## Alpha 1
+
+done
+
+* bootstrap
+
+* gramma #
+
+To-do
+
+* gramma *
+
+* textarea css optimization
+
+## Alpha 2
+
+done 
+
+* angular bower
+
+* using RegExp
+
+To-do
+
+* gramma *
+
+* textarea css optimization
